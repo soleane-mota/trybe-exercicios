1eb6154cda5d9a40565be8e72cf722a2f5b911b6
Soleane Mota <sem.sol@gmail.com>
2022-10-12 17:53:24 -0400

    Resolve a atividade sobre operador AND da pratica4 do dia 1 da secao 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica4-operadores-logicos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica4-operadores-logicos.js
@@ -8,6 +8,25 @@
   // Se o horário estiver entre 4 e 11, insira “Hmmm, cheiro de café recém-passado” na variável “message”.
 // Agora imprima a variável message fora das suas condições.
 
+const currentHour = 12;
+let message = '';
+
+if (currentHour >= 22) {
+	message = 'Não deveríamos	comer nada, é hora de dormir';
+} else if (currentHour < 22 && curretHour >= 18) {
+	message = 'Rango	da noite, vamos jantar :D';
+} else if (currentHour < 18 && curretHour >= 14) {
+	message = 'Vamos	fazer um bolo pro café da tarde?';
+} else if (currentHour < 14 && curretHour >= 11) {
+	message = 'Hora do almoço!!!';
+} else if (currentHour < 11 && curretHour >= 4) {
+	message = 'Hmmm, cheiro de café	recém-passado';
+} else {
+	message = 'Não é hora de comer.'
+}
+
+// console.log(message);
+
 // ====================================================================
 
 // Crie uma variável chamada “weekDay” que receba a string “quarta-feira”.
@@ -15,4 +34,3 @@
 	// Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
 	// Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.
 
-	
