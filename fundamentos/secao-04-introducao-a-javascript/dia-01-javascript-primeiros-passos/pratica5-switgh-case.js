ce050b9316f58857d83601aee52ae830d7629d66
Soleane Mota <sem.sol@gmail.com>
2022-10-12 17:57:48 -0400

    Cria atividade pratica5 do dia 1 da secao 4

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica5-switgh-case.js
@@ -0,0 +1,8 @@
+// Crie uma variável para armazenar o status da pessoa candidata no processo seletivo, que pode ser: 'aprovada', 'lista' ou 'reprovada';
+// Crie uma estrutura condicional com o switch/case que irá imprimir as seguintes mensagens:
+	// Caso 'aprovada', imprima “Parabéns, você foi aprovada(o)!”.
+	// Caso 'lista', imprima “Você está na nossa lista de espera”.
+	// Caso 'reprovada', imprima “Você foi reprovada(o)”.
+	// Caso default, imprima a mensagem de “Informação incorreta”.
+
+	
