1cd5cb3ef35b9f26edb96d8b60f69556919f52b5
Soleane Mota <sem.sol@gmail.com>
2022-10-12 17:55:54 -0400

    Resolve a atividade sobre operador OR da pratica4 do dia 1 da secao 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica4-operadores-logicos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica4-operadores-logicos.js
@@ -34,3 +34,10 @@ if (currentHour >= 22) {
 	// Se nossa variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
 	// Se for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido UwU”.
 
+	let weekDay = 'quarta-feira';
+
+	if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+		console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+	} else {
+		console.log('FINALMENTE, descanso	merecido UwU');
+	}
