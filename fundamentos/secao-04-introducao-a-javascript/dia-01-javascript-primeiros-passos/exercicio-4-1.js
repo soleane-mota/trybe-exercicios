4c8d569b646c20612a849c8c5922e9a8462159ee
Soleane Mota <sem.sol@gmail.com>
2022-10-12 18:57:40 -0400

    Resolve o requisito que determina se eh um triangulo do exercicio 4.1

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4-1.js
@@ -7,6 +7,7 @@
 
 const a = 10;
 const b = 2;
+const c = 20;
 
 // console.log(a + b);
 // console.log(a - b);
@@ -26,6 +27,41 @@ if (a > b) {
 
 // - Utilize `if/else` para fazer um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
 
+if (a > b && a > c) {
+	console.log(`O número ${a} é maior que ${b} e ${c}.`);
+} else if (b > a && b > c) {
+	console.log(`O número ${b} é maior que ${a} e ${c}.`);
+} else if (c > a && c > b) {
+	console.log(`O número ${c} é maior que ${a} e ${b}.`);
+} else {
+	console.log(`O número são iguais.`);
+}
 
 // - Utilize `if/else` para fazer um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+function wholeNumber(num) {
+	if (Math.round(num) >= 0) {
+		return 'positivo';
+	} else {
+		return 'negativo';
+	}
+}
+console.log(wholeNumber(-0.9));
+
 // - Utilize `if/else` para fazer um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne `true` se os ângulos representarem os ângulos de um triângulo e `false`, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+
+function triangle() {
+
+	const a = 180;
+	const b = 90;
+	const c = 1;
+	
+	if ((b - c) < a && a < (b + c) || (a - c) < b && b < (a + c) || (a - b) < c && c < (a + b)) {
+		return true;
+	} else if (typeof(a) !== 'number'  || typeof(b) !== 'number' || typeof(c) !== 'number') {
+		return 'Erro! Valor inválido';
+	} else {
+		return false;
+	}
+}
+console.log(triangle());
