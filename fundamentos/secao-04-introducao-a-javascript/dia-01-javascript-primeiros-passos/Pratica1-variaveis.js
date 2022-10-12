11df69106696a11255642900422ee1c4e271ac62
Soleane Mota <sem.sol@gmail.com>
2022-10-12 17:30:35 -0400

    Resolve a atividade pratica1 do dia 1 da secao 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/Pratica1-variaveis.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/Pratica1-variaveis.js
@@ -3,4 +3,22 @@
 // Crie uma variável let chamada birthYear e atribua a ela o ano em que você nasceu.
 // Utilize o console.log() para imprimir as constantes e variáveis que você criou.
 // Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+const myName = Soleane;
+const birthCity = 'Mirassol D\' Oeste';
+let birthYear = 1987;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
 // Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+
+birthCity = 'Manaus';
+
+// Resposta: Porque é uma constante e não pode ter novo valor atribuído.
+
+console.log(birthCity);
+console.log(birthYear);
