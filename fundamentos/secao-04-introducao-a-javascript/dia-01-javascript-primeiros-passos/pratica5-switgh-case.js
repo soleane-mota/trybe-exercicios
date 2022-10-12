d00fbe3d6a18f9ff8ed3ee1b608239007daef645
Soleane Mota <sem.sol@gmail.com>
2022-10-12 18:05:37 -0400

    Resolve a atividade pratica5 do dia 1 da secao 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica5-switgh-case.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica5-switgh-case.js
@@ -5,4 +5,18 @@
 	// Caso 'reprovada', imprima “Você foi reprovada(o)”.
 	// Caso default, imprima a mensagem de “Informação incorreta”.
 
-	
+	const status = 'reprovada';
+
+	switch (status) {
+		case 'aprovada':
+			console.log('Parabéns, você foi aprovada(o)!');
+			break;
+		case 'lista':
+			console.log('Você está na nossa lista de espera');
+			break;
+		case 'reprovada':
+			console.log('Você foi reprovada(o)');
+			break;
+		default:
+			console.log('Informação incorreta');
+	}
