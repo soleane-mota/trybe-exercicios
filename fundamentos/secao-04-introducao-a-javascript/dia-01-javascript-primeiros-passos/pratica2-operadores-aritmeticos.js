5af66ac14f0c7901283bf85a97e0f0989199edde
Soleane Mota <sem.sol@gmail.com>
2022-10-12 17:36:27 -0400

    Resolve a atividade pratica2 do dia 1 da secao 4

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica2-operadores-aritmeticos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/pratica2-operadores-aritmeticos.js
@@ -2,3 +2,12 @@
 // Crie uma constante uma chamada heigth e atribua a ela o valor 8.
 // Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar ~~sempre~~ o console.log() para imprimir as variáveis e checar os resultados das operações!
 // Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimeter = (base * 2) + (heigth * 2);
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(perimeter);
