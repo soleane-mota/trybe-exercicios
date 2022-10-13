28bcecf5cfadc6f25f552a5cbd5cf668f216e94a
Soleane Mota <sem.sol@gmail.com>
2022-10-12 21:07:37 -0400

    Resolve o requisito dos numeros pares ou impares do exercicio 4.1

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4-1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javascript-primeiros-passos/exercicio-4-1.js
@@ -132,3 +132,22 @@ if (nota < 0 || nota > 100) {
 	console.log('F');
 }
 
+// Utilize if/else para fazer um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
+
+function parOuImpar() {
+	const num1 = 11;
+	const num2 = 7;
+	const num3 = 5;
+	
+	if (num1%2 === 0 || num2%2 === 0 || num3%2 === 0) {
+		return true;
+	} 
+	return false;
+}
+console.log(parOuImpar());
+// Utilize if/else para escrever um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda, descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+	// Atente que, sobre o custo do produto, incide um imposto de 20%.
+	// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+	// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+		// valorCustoTotal = valorCusto + impostoSobreOCusto;
+		// lucro = valorVenda - valorCustoTotal (lucro de um produto);
